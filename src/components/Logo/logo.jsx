a2ee38940e779d101c3b1954535d21e85973debf
i-am-sank <sankthegamer@gmail.com>
2022-02-10 20:13:37 +0530

    initial

---
+++ src/components/Logo/logo.jsx
@@ -0,0 +1,13 @@
+import { Statistic } from "semantic-ui-react"
+
+
+const Logo = () => {
+    return (
+        <Statistic>
+            <Statistic.Value>PEB</Statistic.Value>
+            <Statistic.Label>Pre Elec Board</Statistic.Label>
+        </Statistic>
+    )
+}
+
+export default Logo;
